c8d0e3a0abdd867bd568d9ce8e0a5c31ef5b3fdd
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-19 11:53:05 +0200

    add message model and fix bcrypt issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 require 'faker'
 puts 'Destroying data... 🗑'
+User.destroy_all
+Message.destroy_all
 5.times do 
   User.create(
     username: Faker::Internet.username(specifier: 5..8),
     password: 'helloworld'
   )
 end
-puts '5 user instances created! Your data is ready 🍪'
+
+for i in 1..5
+  Message.create(
+    body: Faker::Quote.yoda,
+    user: User.all.sample
+  )
+end  
+
+puts '5 user/messages instances created! Your data is ready 🍪'
