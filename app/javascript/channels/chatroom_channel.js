f8cce05d01281a861db23ab58898efcadcb9aa44
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-20 17:54:19 +0200

    add broadcasting messages

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,6 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    alert(data.foo)
+    document.getElementById('message-container').insertAdjacentHTML('beforeend', data.mod_message)
   }
 });
